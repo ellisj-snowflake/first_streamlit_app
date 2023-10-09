b1e69a64ed4aa9da3d7cc90d3aaf320350e08660
ellisj-snowflake <145302623+ellisj-snowflake@users.noreply.github.com>
2023-10-08 21:24:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
-streamlit.text("Fruitlist")
-streamlit.text(my_data_row)
+streamlit.header("Fruitlist")
+streamlit.dataframe(my_data_row)
 
 #don't run anything past this point
 streamlit.stop()
