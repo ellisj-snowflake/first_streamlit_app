aa905d28e7dbf317e0f3a7b3d068d1966e63c171
ellisj-snowflake <145302623+ellisj-snowflake@users.noreply.github.com>
2023-10-08 21:14:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,23 @@ except URLError as e:
 
 
 #import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+#don't run anything past this point
+streamlit.stop()
+
+
+
+
+
+
+
+
 
 streamlit.header("This fruit load list contains:")
 #Snowflake-related functions
